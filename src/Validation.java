ce891bc6d7852d8dc5f2c5db199495020b37cb16
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-01-25 22:39:23 -0500

    implementation of methods to check if a string is a number

--- src/Validation.java
+++ src/Validation.java
@@ -8,4 +8,102 @@ public class Validation {
     public static boolean validateSize(String string, int size) {
         return string.length() == size;
     }
+
+    /**
+     * Método estático que valida si un un string es un número entero
+     * @param string String a validar
+     * @return true si el string es un número entero, false si no
+     */
+    public static boolean validateInteger(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Método estático que valida si un un string es un número decimal
+     * @param string String a validar
+     * @return true si el string es un número decimal, false si no
+     */
+    public static boolean validateDouble(String string) {
+        try {
+            Double.parseDouble(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Método estático que valida si un un string es un número entero positivo
+     * @param string String a validar
+     * @return true si el string es un número entero positivo, false si no
+     */
+    public static boolean validatePositiveInteger(String string) {
+        try {
+            int number = Integer.parseInt(string);
+            return number >= 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Método estático que valida si un un string es un número decimal positivo
+     * @param string String a validar
+     * @return true si el string es un número decimal positivo, false si no
+     */
+    public static boolean validatePositiveDouble(String string) {
+        try {
+            double number = Double.parseDouble(string);
+            return number >= 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Método estático que valida si un un string es un número entero negativo
+     * @param string String a validar
+     * @return true si el string es un número entero negativo, false si no
+     */
+    public static boolean validateNegativeInteger(String string) {
+        try {
+            int number = Integer.parseInt(string);
+            return number < 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Método estático que valida si un un string es un número decimal negativo
+     * @param string String a validar
+     * @return true si el string es un número decimal negativo, false si no
+     */
+    public static boolean validateNegativeDouble(String string) {
+        try {
+            double number = Double.parseDouble(string);
+            return number < 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Método estático que valida si un un string es un número entero positivo o cero
+     * @param string String a validar
+     * @return true si el string es un número entero positivo o cero, false si no
+     */
+    public static boolean validatePositiveOrZeroInteger(String string) {
+        try {
+            int number = Integer.parseInt(string);
+            return number >= 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
