53c07e83e33693cdaab53dc553325dae2b7f8510
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-01-25 22:12:56 -0500

    implementation of method to check if a number is within a specific range

--- src/Validation.java
+++ src/Validation.java
@@ -1,3 +1,12 @@
 public class Validation {
-    
+    /**
+     * Método estático que valida si un número está dentro de un rango
+     * @param number Número a validar
+     * @param min Rango mínimo
+     * @param max Rango máximo
+     * @return true si el número está dentro del rango, false si no
+     */
+    public static boolean isNumberInRange(double number, double min, double max) {
+        return number >= min && number <= max;
+    }
 }
