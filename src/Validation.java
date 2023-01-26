2d6de4de98da8ae6bed1a1482a482e15649786bb
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-01-25 22:36:53 -0500

    implementation of method to check if a string is of a specific size

--- src/Validation.java
+++ src/Validation.java
@@ -1,3 +1,11 @@
 public class Validation {
-    
+    /**
+     * Método estático que valida si un un string es de un tamaño determinado
+     * @param string String a validar
+     * @param size Tamaño del string
+     * @return true si el string es del tamaño indicado, false si no
+     */
+    public static boolean validateSize(String string, int size) {
+        return string.length() == size;
+    }
 }
