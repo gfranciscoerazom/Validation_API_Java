6bccec5120debd77e83727a7f398ecd586438949
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-01-25 22:14:52 -0500

    implementation of a method to verify whether a number is positive or negative

--- src/Validation.java
+++ src/Validation.java
@@ -9,4 +9,22 @@ public class Validation {
     public static boolean isNumberInRange(double number, double min, double max) {
         return number >= min && number <= max;
     }
+
+    /**
+     * Método estático que valida si un número es positivo
+     * @param number Número a validar
+     * @return true si el número es positivo, false si no
+     */
+    public static boolean isPositiveNumber(double number) {
+        return number >= 0;
+    }
+
+    /**
+     * Método estático que valida si un número es negativo
+     * @param number Número a validar
+     * @return true si el número es negativo, false si no
+     */
+    public static boolean isNegativeNumber(double number) {
+        return number < 0;
+    }
 }
